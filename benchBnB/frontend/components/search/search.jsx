06c8cd0443f027bc422f1c7aebb5a915fd45dca7
Sam McClure <samkmcclure@gmail.com>
2018-10-08 17:57:09 -0700

    Start working on adding map component

---
+++ benchBnB/frontend/components/search/search.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import BenchMap from '../bench_map/bench_map';
+import BenchIndex from '../bench_index';
+
+class Search extends React.Component {
+
+  render(){
+    return (
+    <div>
+      {BenchMap}
+      {BenchIndex}
+    </div>
+    );
+  }
+}
